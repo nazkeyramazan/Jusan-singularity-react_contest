10e2c81c9a5e36a92e99e7eb345bdce9b3f94e5a
Ramazan Nazkey <nazkeyramazan@gmail.com>
2022-08-05 18:53:54 +0600

    create new Post api request

--- src/components/posts/createPost.tsx
+++ src/components/posts/createPost.tsx
@@ -1,23 +1,36 @@
 import React, {useState} from 'react'
 import {Post} from "./post";
+const axios = require('axios');
 
 export const CreatePost = () => {
     const [userId, setUserId] = useState<number>()
-    const [id, setId] = useState<number>()
     const [title, setTitle] = useState<string>('')
     const [body, setBody] = useState<string>('')
     // const [newPost, setNewPost] = useState<Post>({
     //     userId: 0,
-    //     id: 0,
     //     title: '',
     //     body: ''
     // })
     const handleSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
         console.log('new post is submitted')
         e.preventDefault();
-        console.log(
-            title, body, id, userId
-        )
+        axios.post('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: title,
+                body: body,
+                userId: userId,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(function (response:Post) {
+                console.log(response);
+            })
+            .catch(function (error:string) {
+                console.log(error);
+            })
     }
 
     return (
@@ -27,12 +40,6 @@ export const CreatePost = () => {
                     <label>
                         userId:
                         <input type="number" name='userId' value={userId} onChange={(e)=>setUserId(Number(e.target.value))} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        id:
-                        <input type="number" name='id' value={id} onChange={(e)=>setId(Number(e.target.value))} />
                     </label>
                 </div>
                 <div>
