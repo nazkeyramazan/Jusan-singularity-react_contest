9aa433b6247478121107ef98e71014c3854db47b
Ramazan Nazkey <nazkeyramazan@gmail.com>
2022-08-05 17:58:28 +0600

    add Post page

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
+interface Post {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+interface getPostsReq {
+    data : Post[]
+}
 export const Post = () => {
+    const [posts, setPosts] = useState<Post[]>([])
+
+    useEffect(()=>{
+        const axios = require('axios');
+        // @ts-ignore
+        axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+            .then(function (response:getPostsReq) {
+                console.log(response.data);
+                setPosts(response.data)
+            })
+            .catch(function (error:string) {
+                console.log(error);
+            })
+
+    }, [])
+    // @ts-ignore
     return (
-        <div style={{color:'red'}}>pasdfosts</div>
+        <div >
+            {posts.map((item:Post)=>(
+                <div>
+                    <p >{item.id} </p>
+                    <p >{item.title} </p>
+                    <p >{item.body} </p>
+                </div>
+                ))}
+        </div>
         )
 }
